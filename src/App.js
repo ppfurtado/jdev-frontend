759c046339b3f1fa8305af6bbf4cb61f0c882e78
ppfurtado <furtado.pedropaulo@gmail.com>
2021-06-30 23:28:09 -0300

    add path Home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <h2>CRUD SPRINGBOOT API REST</h2>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
